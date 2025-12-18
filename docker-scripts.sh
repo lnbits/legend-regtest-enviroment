52cb1fec2b3b93e901e052973b5c9e9e306063db
dni ⚡ <office@dnilabs.com>
2025-12-18 08:17:37 +0100

    fix for lnbits

--- docker-scripts.sh
+++ docker-scripts.sh
@@ -2,7 +2,7 @@
 export COMPOSE_PROJECT_NAME=lnbits
 
 boltzcli-sim() {
-  docker exec lnbits-boltz-client-1 boltzcli "$@"
+  docker exec lnbits-boltz-client-1 boltzcli --host "boltz-client" "$@"
 }
 
 bitcoin-cli-sim() {
